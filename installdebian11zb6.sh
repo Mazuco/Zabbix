1d4b313f4a3e220ad186e86fdc4a0dcbc89e0124
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2023-09-13 11:42:42 -0300

    Instalação do pacote lsb-release
    O pacote lsb-release é uma dependência do pacote mysql-apt-config

--- installdebian11zb6.sh
+++ installdebian11zb6.sh
@@ -39,6 +39,8 @@ set -e
 
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.20-1_all.deb
 
+apt-get install lsb-release -y
+
 dpkg -i mysql-apt-config_*_all.deb
 
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 467B942D3A79BD29
