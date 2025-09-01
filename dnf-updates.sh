d5dbdecf25682d1b5e6002c94b018d84a889bc97
Vitor Mazuco <vitor.mazuco@gmail.com>
2025-09-01 08:54:48 -0300

    Add files via upload

---
+++ dnf-updates.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# Autor: Vitor Mazuco
+# Descrição: Script para monitorar updates no Rocky/RHEL e enviar para Zabbix
+# dnf-updates.sh
+# Modificado em: 01/09/2025
+
+ZABBIX_HOST=$(hostname)
+SECURITY_UPDATES=$(dnf updateinfo list security updates -q | grep -cE '^[A-Z]')
+TOTAL_UPDATES=$(dnf check-update --quiet | grep -cE '^[a-zA-Z0-9]')
+DATE_RUN=$(date +%s)
+
+# Saída no formato esperado pelo zabbix_sender
+echo "- yum.updates.total[$ZABBIX_HOST] $TOTAL_UPDATES"
+echo "- yum.updates.security[$ZABBIX_HOST] $SECURITY_UPDATES"
+echo "- yum.updates.lastcheck[$ZABBIX_HOST] $DATE_RUN"
+
+
+
+
+
+
+
